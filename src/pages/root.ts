1f8ee68c66fbb87e836c5685c2b8f3c8583fdda9
evandrouzeda <evandrouzeda@gmail.com>
2023-12-04 20:57:27 +0000

    adicionando container e subgrid

--- src/pages/root.ts
+++ src/pages/root.ts
@@ -1,8 +1,9 @@
 import Z, { Zeyo } from "zeyo";
 import Page from ".";
 import style from "./inicio.module.css";
+import container from "./container.module.css";
 import L from "leaflet";
-
+declare const marked: any;
 export default class Root extends Page {
     route: string = "/"
     title?: string | undefined;
@@ -22,8 +23,11 @@ export default class Root extends Page {
             }).addTo(map);
             console.log(map)
         })();
-        return this.main = Z("main").class("d-grid", "jc-center").children(
-            Z("header").class(style.header).children(
+        const clipboard = new window.ClipboardEvent("text")
+        const text = clipboard.clipboardData
+        console.log(text)
+        return this.main = Z("main").class(container.grid).children(
+            Z("header").class(style.header, container.breakout).children(
                 Z("img").attribute("src", "https://abiblicapf.org/_img/logo-grande.png"),
                 Z("nav").class("d-flex", "jc-between").children(
                     Z("a").text("Início"),
@@ -31,42 +35,54 @@ export default class Root extends Page {
                     Z("a").text("Membro"),
                 ),
             ),
-            Z("section").class(style.container, "txt-img").children(
-                Z("h2").class(style.primary, style["first-text"]).children(
+            Z("section").class(style.hero, "txt-img").children(
+                Z("h2").class(style.primary).children(
                     `Uma `,
                     Z("span").class(style.second).text("Família"),
                     ` de servos de `,
                     Z('span').class(style.second).text("Jesus Cristo"),
                     `, organizados em Células`
                 ),
-                Z("img").class(style["second-img"]).attribute("style", "transform: scaleX(-1);")
+                Z("img").attribute("style", "transform: scaleX(-1);")
                     .attribute("src", "https://images.unsplash.com/photo-1517486808906-6ca8b3f04846?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80"),
             ),
-            Z("section").class(style.container, style.overflow, "txt-bg-img").children(
-                Z("img").class(style["center-img"]).set("src", "/_img/biblia.png"),
-                Z("div").class(style["center-img"], style["center-gradient"]),
-                Z("h2").class(style.primary, style["center-text"]).children(`Vivendo segundo a `, Z("span").class(style.second).text("Bíblia"))
+            Z("section").class(container["full-width"], style.smash, "txt-bg-img").children(
+                Z("img").class(container.breakout2,).set("src", "/_img/biblia.png"),
+                Z("div").class(container.breakout2, style["center-gradient"]),
+                Z("h2").class(container.free, style["center-text"], style.primary).children(`Vivendo segundo a `, Z("span").class(style.second).text("Bíblia"))
             ),
-            Z("section").class(style.container, "img-txt").children(
-                Z("div").class(style.shadow),
-                Z("img").class(style["left-img"]).attribute("src", "https://images.unsplash.com/photo-1572154302589-f29e67d304d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
-                Z("h2").class(style["right-text"]).children(
+            Z("section").class(/* style.container, */ "img-txt").children(
+                Z("div").class(/* style.shadow */),
+                Z("img").class(/* style["left-img"] */).attribute("src", "https://images.unsplash.com/photo-1572154302589-f29e67d304d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
+                Z("h2").class(/* style["right-text"] */).children(
                     `Preparando as pessoas a alcançar os `,
-                    Z("span").class(style["second-tint"]).text("cansados"),
+                    Z("span").class(/* style["second-tint"] */).text("cansados"),
                     ` e `,
-                    Z("span").class(style["second-tint"]).text("sobrecarregados"),
+                    Z("span").class(/* style["second-tint"] */).text("sobrecarregados"),
                     `,`
                 )
             ),
-            Z("section").class(style.container, style.overflow, "txt-bg-img").children(
-                Z("h2").class(style["center-text-l"]).children(`através dos relacionamentos pessoais visando a `, Z("span").class(style.third).text("multiplicação"))
+            Z("section").class(/* style.container, style.overflow, */ "txt-bg-img").children(
+                Z("h2").class(/* style["center-text-l"] */).children(`através dos relacionamentos pessoais visando a `, Z("span").class(/* style.third */).text("multiplicação"))
             ),
             Z("footer").children(
                 Z("div"),
                 Z("div"),
                 Z("div"),
             ),
-            Z("div").set("id", "map").set("style", (" height: 600px; width: 100%" as any))
+            Z("div").set("id", "map").set("style", (" height: 600px; width: 100%" as any)),
+            Z("section").class("d-flex").object(o => {
+                const content = Z("div")
+                o.children(
+                    Z("textarea").object((input) => {
+                        window.addEventListener("keyup", () => {
+                            console.log(input.element.value)
+                            content.element.innerHTML = marked.parse(input.element.value)
+                        })
+                    }),
+                    content
+                )
+            }),
         )
     }
 }
